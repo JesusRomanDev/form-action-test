733b541a2f12add143880cfcc95b55b069a73713
Jesus Roman <jesus_fime@outlook.es>
2024-07-28 16:54:22 -0600

    New files

--- backend/index.js
+++ backend/index.js
@@ -9,17 +9,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 //Para servir archivos estaticos en la raiz y poder leer index y submit html
+
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'index.html'));
+// });
+
 // app.use(express.static(path.join(__dirname, '..')));
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'index.html'));
-});
-
 // app.get('/submit', (req, res) => {
 //     console.log('Llegaste');
-//     res.redirect('/submit.html');
+//     res.redirect('/nueva.html');
 // });
+
+app.get('/submit.html', (req, res) => {
+    console.log('Hola');
+    // res.redirect('/indexx.html');
+})
 
 app.listen(port, ()=>{
     console.log(`El servidor esta funcionando en el puerto ${port} `);
